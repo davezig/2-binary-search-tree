3d6e2a9c6e792e9ae1caabba694efa3e30b2ec37
bamgithub <48144766+bamgithub@users.noreply.github.com>
2020-12-29 14:00:58 -0500

    first commit with work

--- problems/01-bst.js
+++ problems/01-bst.js
@@ -7,6 +7,53 @@ class TreeNode {
 }
 
 class BST {
+  constructor() {
+    this.root = null;
+  }
+  
+  insert(val, root=this.root) {
+    if(!this.root) {
+      this.root = new TreeNode(val);
+      return; 
+    }
+    if(val < root.val) {
+      if (!root.left) {
+        root.left = new TreeNode(val);
+      }
+      else {
+        this.insert(val, root.left);
+      }
+    }
+    else {
+      if (!root.right) {
+        root.right = new TreeNode(val);
+      }
+      else {
+        this.insert(val, root.right);
+      }
+    }
+  }
+  
+  searchRecur(val, root=this.root){
+    if(!root) return false;
+    
+    if (val < root.val) {
+      return this.searchRecur(val, root.left);
+    }
+    else if (val > root.val) {
+      return this.searchRecur(val, root.right);
+    }
+    else {
+      return true;
+    }
+  }
+
+  searchIter(val, root=this.root) {
+    if(!root) return false;
+
+    if (val) return true;
+    else return false;
+  }
 
 }
 
--- problems/02-findMin.js
+++ problems/02-findMin.js
@@ -1,5 +1,5 @@
 function findMin(root) {
-
+  if (!root) return null;
 }
 
 
